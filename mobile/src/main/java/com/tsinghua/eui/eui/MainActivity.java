54c182619c0a978b03076ef641e172f6432bf0b9
徐呈寅 <xuchengy16@mails.tsinghua.edu.cn>
2018-11-08 21:41:28 +0800

    setup project

---
+++ mobile/src/main/java/com/tsinghua/eui/eui/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tsinghua.eui.eui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
